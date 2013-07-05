e3032f73a7e3cea20b2bf1e37c6e22ed459cb90a
Michael Robinson <mike@pagesofinterest.net>
2013-07-05 23:56:12 +1200

    Added initialisation script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   addPermission: require('./lib/functions/permissions.js').addPermission,
   permission: require('./lib/middleware/permission'),
-  authentication: require('./lib/middleware/authentication.js')
+  authentication: require('./lib/middleware/authentication.js'),
+  initialise: require('./lib/initialise.js')
 }
