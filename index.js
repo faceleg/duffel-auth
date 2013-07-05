c55a21f81573dd94835650d57c5022510818bb86
Michael Robinson <mike@pagesofinterest.net>
2013-07-05 23:19:44 +1200

    Fixed issues in index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-var connect = require('connect');
-
-var permissions = {
-  ''
-}
-// add permission first, then authorisation.
-// declare permissions like:
-{
-  route: '/abc',
-  login: true,
-  permission: 'some-permission'
-}
-
 module.exports = {
-  addPermission: require('./functions/permissions.js').addPermission,
-  permission: require('./middleware/permission'),
-  authorise: require('./middleware/authentication.js')
+  addPermission: require('./lib/functions/permissions.js').addPermission,
+  permission: require('./lib/middleware/permission'),
+  authentication: require('./lib/middleware/authentication.js')
 }
