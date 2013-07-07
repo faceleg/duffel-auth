b2d72b8ec1611bc9898f50f05a9fe04015d313cf
Michael Robinson <mike@pagesofinterest.net>
2013-07-08 08:47:01 +1200

    Loading feedback

--- public/javascript/applications/login/controllers.js
+++ public/javascript/applications/login/controllers.js
@@ -3,15 +3,19 @@
 function LoginController($scope, $http, $window) {
   $scope.user = {};
   $scope.failedOnce = false;
+  $scope.submitting = false;
+
   $scope.login = function() {
     $scope.error = false;
+    $scope.submitting = true;
     $http.post('/login', $scope.user).
       success(function(data, status, headers, config) {
         $window.location.href = '/dashboard';
-    }).
+      }).
       error(function(data, status, headers, config) {
         $scope.error = status;
         $scope.failedOnce = true;
+        $scope.submitting = false;
     });
   };
 };
