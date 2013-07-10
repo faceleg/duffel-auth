f36be2e82ad3dcde87702c4170ae3312c62f737d
Michael Robinson <mike@pagesofinterest.net>
2013-07-10 22:00:30 +1200

    Export model and schema

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
   addPermission: require('./lib/functions/permissions').addPermission,
-  initialise: require('./lib/initialise')
+  initialise: require('./lib/initialise'),
+  User: require('./models/User').model,
+  UserSchema: require('./models/User').schema
 }
