b479b3cd24b15295be078a5998e5841f664f9a94
Michael Robinson <mike@pagesofinterest.net>
2013-07-12 23:11:57 +1200

    Do not export user or schema, both available on the relevant connection

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = {
   addPermission: require('./lib/functions/permissions').addPermission,
   initialise: require('./lib/initialise'),
-  User: require('./lib/models/User').model,
-  UserSchema: require('./lib/models/User').schema
 }
