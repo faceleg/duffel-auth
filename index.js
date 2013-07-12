9674888c8659f489003d9e5aa1a24451bde49173
Michael Robinson <mike@pagesofinterest.net>
2013-07-13 00:59:03 +1200

    Expose User model with a getter to ensure it is associated to the correct connection

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
   addPermission: require('./lib/functions/permissions').addPermission,
   initialise: require('./lib/initialise'),
+  User: function() {
+    return require('./lib/models/User').model()
+  }
 }
