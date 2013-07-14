7dc45d7a99440b16975671ce4f9387def3e9252b
Michael Robinson <mike@pagesofinterest.net>
2013-07-14 23:15:57 +1200

    Added token model

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
   initialise: require('./lib/initialise'),
   addPermission: require('./lib/functions/permissions').addPermission,
+  Token: function() {
+    return require('./lib/models/Token').model()
+  },
   User: function() {
     return require('./lib/models/User').model()
   }
