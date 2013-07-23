6fc6315a81e9c0e7b7c7d0a650d409f6c88dc37c
Michael Robinson <mike@pagesofinterest.net>
2013-07-23 23:50:17 +1200

    Moved token to own module

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 module.exports = {
   initialise: require('./lib/initialise'),
   addPermission: require('./lib/functions/permissions').addPermission,
-  Token: function() {
-    return require('./lib/models/Token').model()
-  },
   User: function() {
     return require('./lib/models/User').model()
   }
