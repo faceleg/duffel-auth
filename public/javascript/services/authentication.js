a9f34d589dd8469c1eed3c6d30bbd65705a5ffad
Michael Robinson <mike@pagesofinterest.net>
2013-08-14 00:34:36 +1200

    Overhauled authentication service

--- public/javascript/services/authentication.js
+++ public/javascript/services/authentication.js
@@ -1,57 +1,68 @@
 'use strict';
+(function() {
+  var currentUser = false;
+  angular.module('authentication', ['user'])
 
-angular.module('authentication', ['user'])
+    .factory('authenticationService', [
+      'User', '$rootScope', '$http', '$timeout', function(User, $rootScope, $http, $timeout) {
+      var loggedIn = false;
 
-  .factory('authenticationService', ['User', '$rootScope', '$http', function(User, $rootScope, $http) {
-    var loggedIn = false;
+      var POLL_FREQUENCY = 5 * 60 * 1000;
+      var checkLoginState = function() {
+        $http.head('/duffel-auth/api/login-state')
+          .success(function() {
+            $timeout(checkLoginState, POLL_FREQUENCY);
+          });
+      }
 
-    var POLL_FREQUENCY = 5 * 60 * 1000;
-    var checkLoginState = function() {
-      $http.head('/api/login-state')
-        .success(function() {
+      if (!currentUser) {
+        currentUser = User.current(function() {
+          loggedIn = true;
+          $rootScope.$broadcast('event:auth-loginConfirmed', currentUser);
           $timeout(checkLoginState, POLL_FREQUENCY);
         });
-    }
-
-    var currentUser = User.current(function() {
-      loggedIn = true;
-      $rootScope.$broadcast('event:auth-loginConfirmed', currentUser);
-      $timeout(checkLoginState, POLL_FREQUENCY);
-    });
-
-    return {
-      currentUser: function() {
-        return currentUser;
-      },
-      isLoggedIn: function() {
-        return loggedIn;
-      }
-    };
-  }])
-
-  /**
-   * $http interceptor.
-   * On 401 response  broadcasts 'event:angular-auth-loginRequired'.
-   */
-  .config(['$httpProvider', function($httpProvider) {
-
-    var interceptor = ['$rootScope', '$q', function($rootScope, $q) {
-      function success(response) {
-        return response;
       }
 
-      function error(response) {
-        if (response.status === 401) {
-          $rootScope.$broadcast('event:auth-loginRequired');
+      $rootScope.$on('event:auth-loginRequired', function() {
+        currentUser = false;
+      });
+
+      return {
+        currentUser: function() {
+          return currentUser;
+        },
+        isLoggedIn: function() {
+          return loggedIn;
+        },
+        login: function(credentials) {
+          currentUser = User.login(credentials);
         }
-        return $q.reject(response);
-      }
-
-      return function(promise) {
-        return promise.then(success, error);
       };
+    }])
 
-    }];
-    $httpProvider.responseInterceptors.push(interceptor);
-  }]);
+    /**
+    * $http interceptor.
+    * On 401 response  broadcasts 'event:auth-loginRequired'.
+    */
+    .config(['$httpProvider', function($httpProvider) {
 
+      var interceptor = ['$rootScope', '$q', function($rootScope, $q) {
+        function success(response) {
+          return response;
+        }
+
+        function error(response) {
+          if (response.status === 401) {
+            $rootScope.$broadcast('event:auth-loginRequired');
+          }
+          return $q.reject(response);
+        }
+
+        return function(promise) {
+          return promise.then(success, error);
+        };
+
+      }];
+      $httpProvider.responseInterceptors.push(interceptor);
+    }]);
+})();
