f38ce16aeec62e6fa3b4ad87bee5eaad41e71b08
Michael Robinson <mike@pagesofinterest.net>
2013-08-14 22:14:55 +1200

    Reverted currentUser caching - back to duplicate current user requests on page load.

--- public/javascript/services/authentication.js
+++ public/javascript/services/authentication.js
@@ -1,52 +1,52 @@
 'use strict';
-(function() {
-  var currentUser = false;
-  angular.module('authentication', ['user'])
+angular.module('authentication', ['user'])
 
-    .factory('authenticationService', [
-      'User', '$rootScope', '$http', '$timeout', function(User, $rootScope, $http, $timeout) {
-      var loggedIn = false;
+.factory('authenticationService', [
+    'User', '$rootScope', '$http', '$timeout',
+  function(User, $rootScope, $http, $timeout) {
+    var loggedIn = false;
 
-      var POLL_FREQUENCY = 5 * 60 * 1000;
-      var checkLoginState = function() {
-        $http.head('/duffel-auth/api/login-state')
-          .success(function() {
-            $timeout(checkLoginState, POLL_FREQUENCY);
-          });
-      }
-
-      if (!currentUser) {
-        currentUser = User.current(function() {
-          loggedIn = true;
-          $rootScope.$broadcast('event:auth-loginConfirmed', currentUser);
-          $timeout(checkLoginState, POLL_FREQUENCY);
-        });
-      }
-
-      $rootScope.$on('event:auth-loginRequired', function() {
-        currentUser = false;
+    var POLL_FREQUENCY = 5 * 60 * 1000;
+    var checkLoginState = function() {
+      $http.head('/duffel-auth/api/login-state')
+        .success(function() {
+        $timeout(checkLoginState, POLL_FREQUENCY);
       });
+    }
 
-      return {
-        currentUser: function() {
-          return currentUser;
-        },
-        isLoggedIn: function() {
-          return loggedIn;
-        },
-        login: function(credentials, error) {
-          currentUser = User.login(credentials, error);
-        }
-      };
-    }])
+    var currentUser = User.current(function() {
+      loggedIn = true;
+      $rootScope.$broadcast('event:auth-loginConfirmed', currentUser);
+      $timeout(checkLoginState, POLL_FREQUENCY);
+    });
 
-    /**
-    * $http interceptor.
-    * On 401 response  broadcasts 'event:auth-loginRequired'.
-    */
-    .config(['$httpProvider', function($httpProvider) {
+    $rootScope.$on('event:auth-loginRequired', function() {
+      currentUser = false;
+    });
 
-      var interceptor = ['$rootScope', '$q', function($rootScope, $q) {
+    return {
+      currentUser: function() {
+        return currentUser;
+      },
+      isLoggedIn: function() {
+        return loggedIn;
+      },
+      login: function(credentials, error) {
+        currentUser = User.login(credentials, error);
+      }
+    };
+  }
+])
+
+/**
+ * $http interceptor.
+ * On 401 response  broadcasts 'event:auth-loginRequired'.
+ */
+  .config(['$httpProvider',
+  function($httpProvider) {
+
+    var interceptor = ['$rootScope', '$q',
+      function($rootScope, $q) {
         function success(response) {
           return response;
         }
@@ -62,7 +62,8 @@
           return promise.then(success, error);
         };
 
-      }];
-      $httpProvider.responseInterceptors.push(interceptor);
-    }]);
-})();
+      }
+    ];
+    $httpProvider.responseInterceptors.push(interceptor);
+  }
+]);
