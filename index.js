5ed06bf588f6cf7d33472870593a104e79275f68
Michael Robinson <mike@pagesofinterest.net>
2013-08-25 22:06:04 +1200

    Added permissionTree

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   initialise: require('./lib/initialise'),
   addPermission: require('./lib/functions/permissions').addPermission,
+  permissionTree: require('./lib/functions/permissions').permissionTree,
   User: function() {
     return require('./lib/models/User').model()
   },
