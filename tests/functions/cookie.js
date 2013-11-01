debde37f07e255a8563ff86b5839c10c7fc8fe3f
Michael Robinson <mike@pagesofinterest.net>
2013-11-02 03:30:20 +1300

    Add parseRedirect tests

--- tests/functions/cookie.js
+++ tests/functions/cookie.js
@@ -32,4 +32,51 @@ describe('Coookie', function() {
 
     });
   });
+
+  describe('parseRedirect', function() {
+
+    it('should return the cookie\'s value if set', function() {
+
+      var testUri = '/test/uri';
+      var cookieValue = cookieFunctions.parseRedirect({
+        cookies: {
+          'duffel-auth-redirect': testUri
+        }
+      }, {
+        clearCookie: function() {}
+      });
+
+      cookieValue.should.have.type('string');
+      cookieValue.should.equal(testUri);
+
+      cookieValue = cookieFunctions.parseRedirect({
+        cookies: {}
+      }, {
+        clearCookie: function() {}
+      });
+
+      cookieValue.should.be.false;
+    });
+
+    it('should return the false if no cookie has been set', function() {
+
+      var cookieValue = cookieFunctions.parseRedirect({
+        cookies: {}
+      }, {
+        clearCookie: function() {}
+      }).should.be.false;
+    });
+
+
+    it('should clear the cookie before returning it', function() {
+
+      cookieFunctions.parseRedirect({
+        cookies: {}
+      }, {
+        clearCookie: function(redirect_cookie_name) {
+          done();
+        }
+      });
+    });
+  });
 });
