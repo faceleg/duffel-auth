8b202714b5d567c8270e15361b514244cdd64190
Michael Robinson <mike@pagesofinterest.net>
2013-12-09 01:10:38 +1300

    Minor refactor of login controller

--- public/javascript/applications/login/controllers.js
+++ public/javascript/applications/login/controllers.js
@@ -1,34 +1,38 @@
-'use strict';
+(function() {
+  'use strict';
 
-function LoginController($scope, User, $window, $cookies, $cookieStore) {
-  $scope.user = {};
-  $scope.submitting = false;
+  angular.module('login.controllers', [])
+    .controller('LoginController', [
+      '$scope', 'User', '$window', '$cookies', '$cookieStore', function LoginController($scope, User, $window, $cookies, $cookieStore) {
 
-  $scope.$on('event:auth-loginConfirmed', function() {
-    var REDIRECT_COOKIE = 'duffel-auth-redirect',
-      redirectUri = $cookies[REDIRECT_COOKIE];
-
-    if (redirectUri) {
-      $cookieStore.remove(REDIRECT_COOKIE);
-      return $window.location.href = redirectUri;
-    }
-    if ($window.location.pathname == '/login') {
-      return $window.location.href = $scope.redirectUri;
-    }
-    $window.location.reload(true);
-  });
-
-  $scope.login = function() {
+    $scope.user = {};
+    $scope.submitting = false;
     $scope.error = false;
-    $scope.submitting = true;
-    User.login($scope.user, function(error, status) {
-      $scope.error = status;
-      $scope.submitting = false;
-    });
-  };
-};
 
-angular.module('login.controllers', [])
-  .controller('LoginController', [
-    '$scope', 'User', '$window', '$cookies', '$cookieStore', LoginController
-]);
+    $scope.$on('event:auth-loginConfirmed', function() {
+      var REDIRECT_COOKIE = 'duffel-auth-redirect',
+        redirectUri = $cookies[REDIRECT_COOKIE];
+
+      if (redirectUri) {
+        $cookieStore.remove(REDIRECT_COOKIE);
+        $window.location.href = redirectUri;
+        return;
+      }
+      if ($window.location.pathname == '/login') {
+        $window.location.href = $scope.redirectUri;
+        return;
+      }
+      $window.location.reload(true);
+    });
+
+    $scope.login = function() {
+      $scope.error = false;
+      $scope.submitting = true;
+      User.login($scope.user, function(error, status) {
+        $scope.error = status;
+        $scope.submitting = false;
+      });
+    };
+  }
+  ]);
+})();
