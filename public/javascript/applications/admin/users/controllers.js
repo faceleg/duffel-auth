1b95f73a017aacead5c53fbeb1b8835b1094aef0
Michael Robinson <mike@pagesofinterest.net>
2014-06-16 12:37:38 +1200

    Moved controller out of controllers

--- public/javascript/applications/admin/users/controllers.js
+++ public/javascript/applications/admin/users/controllers.js
@@ -1,32 +1,3 @@
-'use strict';
-
-function IndexController($scope, User) {
-  $scope.user = User;
-  $scope.headers = [
-    {
-      title: 'Name',
-      property: 'name',
-      template: '<a href="/users/admin/user/{{ item.id }}">{{ item.name }}</a>'
-    },
-    {
-      title: 'Email',
-      property: 'email'
-    },
-    {
-      title: 'Status',
-      property: 'status'
-    }
-  ];
-};
-
-function EditController($scope, User) {
-
-}
-
-angular.module('admin-users.controllers', [])
-  .controller('IndexController', [
-    '$scope', 'User', IndexController
-  ])
-  .controller('EditController', [
-    '$scope', 'User', EditController
-  ]);
+(function() {
+  angular.module('admin-users.controllers', []);
+})();
